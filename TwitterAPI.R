0093abf4ed8faef2da3b70c3cb6b2e23e11d1433
Juan C. Correa <j.correa.n@gmail.com>
2021-12-02 18:58:10 -0500

    Teaching Material

--- TwitterAPI.R
+++ TwitterAPI.R
@@ -1,13 +1,18 @@
 library(twitteR)
-consumerKey <- "wXvqi1I8hNmTsXLttgXupiRQ2"
-consumerSecret <- "ZAKnxv2ZNmfoIh4l3GsRg1MH1aGzsm23E8Kmb8Nkm8PXzyCUg7"
-accessToken <- "106483037-xXczaEith1UuawtlbJt8zQ1M5I61sKhaKkJZQs5W"
-accessTokenSecret <- "tfDXjQMLzJ20pzhc2e35FOzIbvVFt4TA3GR60fZpgKdNu"
+consumerKey <- "sXw2U0g2NjprKWqJnW7Smt3iR"
+consumerSecret <- "ONzRnrmc7BVHbakKBcXBHmIP1LxcGmkyKfsTaT53GCVR1iswX3"
+accessToken <- "106483037-636HLJ1Uq1iA4XBtXijkgt2laX3Uoy6oEactvogs"
+accessTokenSecret <- "LSDvtQ3GvomVI0weUrrhjMpACpvVGqGxomFkFpK5YFXW2"
 
 setup_twitter_oauth(consumerKey, consumerSecret, accessToken,
                     accessTokenSecret)
 2
 
 tweets = searchTwitter("@PachoSantosC", n = 1000)
+tweet2 = searchTwitter("@CaracolRadio", n = 1000)
+tweet3 = searchTwitter("@", n = 1000)
 library(plyr)
 tweets.df = ldply(tweets, function(t) t$toDataFrame())
+tweets.df2 = ldply(tweet2, function(t) t$toDataFrame())
+
+todas = rbind(tweets.df, tweets.df2)
